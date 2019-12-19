45bfb999c19622904d47fd6fa61eb20aa74d7dda
Eric murray <ericwmurray1994@gmail.com>
2019-12-19 06:03:11 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -6,9 +6,10 @@ def create_an_array
   my_array = [1,2,3,4]
 end
 
-def add_element_to_end_of_array 
+def add_element_to_end_of_array
   second_array = ["Wow","I","am","really","learning"]
   second_array << "arrays"
+  puts second_array
 end
 
 def add_element_to_start_of_array 
