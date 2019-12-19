0bb6e17f6d3fe033b6ac6f10e8629d11394f925c
Eric murray <ericwmurray1994@gmail.com>
2019-12-19 06:10:57 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,5 +1,5 @@
 def create_an_empty_array
-  my_empty_array = []
+   []
 end
 
 def create_an_array
@@ -7,6 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array
+  my_array = [1,2,3,4]
+  my_array << 5
 end
 
 def add_element_to_start_of_array 
