d34da99862446cc03aff11ef83390df4cbe5efd8
Eric murray <ericwmurray1994@gmail.com>
2019-12-19 05:48:24 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,9 +1,7 @@
-def create_an_empty_array
-  
+def create_an_empty_array[]
 end
 
-def create_an_array
-  
+def create_an_array["My array","Second string"]
 end
 
 def add_element_to_end_of_array(array, element)
