67296ebdba4d27b450c69eca50434a610ef5b661
Eric murray <ericwmurray1994@gmail.com>
2019-12-19 06:25:52 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -28,7 +28,7 @@ end
 
 def retrieve_element_from_index(array, index_number)
   my_array ["wow","I","am","really","learning","arrays!"]
-  index_number.2 
+  index.2 
 end
 
 def retrieve_first_element_from_array(array)
